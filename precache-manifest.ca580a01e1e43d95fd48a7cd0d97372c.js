373baf19561ad785dcf53cc6e31e36c0b9d4d2d7
Darren <ninenumbproductions@gmail.com>
2018-11-08 15:49:52 -0800

    Updates

---
+++ precache-manifest.ca580a01e1e43d95fd48a7cd0d97372c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0ee6e831e924f823409b",
+    "url": "/go-react-fib/static/js/runtime~main.0ee6e831.js"
+  },
+  {
+    "revision": "6f82784977df12162c46",
+    "url": "/go-react-fib/static/js/main.6f827849.chunk.js"
+  },
+  {
+    "revision": "df98c93a9a94882bc677",
+    "url": "/go-react-fib/static/js/1.df98c93a.chunk.js"
+  },
+  {
+    "revision": "6f82784977df12162c46",
+    "url": "/go-react-fib/static/css/main.9e56da13.chunk.css"
+  },
+  {
+    "revision": "df98c93a9a94882bc677",
+    "url": "/go-react-fib/static/css/1.e217cc17.chunk.css"
+  },
+  {
+    "revision": "78ddfd8ea928daf84d0f51ab619922cc",
+    "url": "/go-react-fib/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/go-react-fib/precache-manifest.ca580a01e1e43d95fd48a7cd0d97372c.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/go-react-fib/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
